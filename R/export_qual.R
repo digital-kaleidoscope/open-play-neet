858e2614f3774bda052617023402761be1cfc70d
nick.ballou@gmail.com <nick.ballou@gmail.com>
2026-06-25 16:42:06 +0200

    tweak plots

--- R/export_qual.R
+++ R/export_qual.R
@@ -5,3 +5,23 @@ positives <- biweekly |>
   select(positives)
 
 nrow(biweekly |> filter(!is.na(problematic_play)))
+
+first_export <- read_csv("R/qual.csv")
+
+qual <- dat_matched |>
+  left_join(biweekly, by = "pid") |>
+  filter(!problematic_play %in% c(NA, "") & !positives %in% c(NA, "")) |>
+  select(
+    pid,
+    wave,
+    age,
+    gender,
+    country,
+    neet,
+    employment,
+    positives,
+    problematic_play
+  ) |>
+  anti_join(first_export, by = c("pid", "wave"))
+
+write_excel_csv(qual, "R/qual2.csv")
